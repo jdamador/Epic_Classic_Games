e7f79833cec2673b13d4659f020520408744a51b
jdamador <amadorsalasjd@gmail.com>
2018-09-20 13:43:04 -0600

    feat: add methods to add new world

--- Hangman/src/codeapp/controller/MainApp_Controller.java
+++ Hangman/src/codeapp/controller/MainApp_Controller.java
@@ -11,13 +11,23 @@ import codeapp.view.MainApp;
 import codeapp.view.Settings;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 
 public class MainApp_Controller implements ActionListener {
+    /**************************************************************************\
+     * Variables declaration.
+    \**************************************************************************/
     MainApp mainApp;
     LettersSoup_Controller soupController = new LettersSoup_Controller();
     Hangman_Controller hangmanController = new Hangman_Controller();
     Settings_Controller settingsController = new Settings_Controller();
+    public ArrayList<String> hangmanWords = new ArrayList<>();
+    public ArrayList<String> lettesSoupWords = new ArrayList<>();
+    /**************************************************************************\
+     *  Constructor. Initialize the variables.
+     * @param mainApp 
+    \**************************************************************************/
     public MainApp_Controller(MainApp mainApp){
        this.mainApp=mainApp;
        this.mainApp.btnHangman.addActionListener(this);
@@ -53,10 +63,13 @@ public class MainApp_Controller implements ActionListener {
         else
             openSoup(mainApp);
     }
-
+    /**************************************************************************\
+     * This method open a new window edit the programs settings
+     * @param mainApp mainApp window.
+    \**************************************************************************/
     private void openSettings(MainApp mainApp) {
         Settings settings = new Settings();
-        settingsController.openSettings(mainApp, settings);
+        settingsController.openSettings(mainApp, settings,hangmanWords);
     }
 
     
--- Hangman/src/codeapp/controller/Settings_Controller.java
+++ Hangman/src/codeapp/controller/Settings_Controller.java
@@ -12,44 +12,89 @@ import codeapp.view.Settings;
 import codeapp.view.WordsSoup;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 
 public class Settings_Controller implements ActionListener{
+   /***************************************************************************\
+    * Global variables declaration.
+   \***************************************************************************/
    private MainApp mainApp;
    private Settings settings;
-   public void openSettings(MainApp mainApp, Settings settings) {
+   private DefaultListModel<String> modelHangman = new DefaultListModel<>();
+   /***************************************************************************\
+    * Open settings window
+    * @param mainApp
+    * @param settings
+    * @param words 
+    \**************************************************************************/
+    public void openSettings(MainApp mainApp, Settings settings, ArrayList<String> words) {
        this.mainApp=mainApp;
        this.settings=settings;
        this.settings.setVisible(true);
        this.mainApp.setVisible(false);
+       managerMethods();
     }
-   public void goBack(){
+    /**************************************************************************\
+     * Call other methods in their respective order.
+    \**************************************************************************/
+    public void managerMethods(){
+        setActionListener();
+    }
+    /**************************************************************************\
+     * Close this window and open the befero window.
+    \**************************************************************************/
+    public void goBack(){
         mainApp.setVisible(true);
         settings.setVisible(false);
     }
-
+    /**************************************************************************\
+     * Method that handle of the events that are produced when some button is
+     * pressed.
+    \**************************************************************************/
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton pushed= (JButton) e.getSource(); 
-        if(pushed.getText().equals("Back"))
+        if(pushed.getToolTipText().equals("Back"))
           goBack();
-        else if(pushed.getText().equals("AddHang"))
+        else if(pushed.getToolTipText().equals("addHang"))
             addNewWord();
-        else if(pushed.getText().equals("CleanHang"))
-            clear();
+        else if(pushed.getToolTipText().equals("CleanHang"))
+            clear(settings.ltSoup,modelHangman,mainApp.mainApp_Controller.hangmanWords);
       }
-    
+    /**************************************************************************\
+     * Set new action listener to handle events
+    \**************************************************************************/
     private void setActionListener() {
         settings.btnBack.addActionListener(this);
         settings.btnCleanHangman.addActionListener(this);
         settings.btnAddHangman.addActionListener(this);
     }
-
+    /**************************************************************************\
+     * Add a new word into the hangman list
+     * Before add a new word is evaluate if this new word contains numbers or
+     * characters.
+    \**************************************************************************/
     private void addNewWord() {
-       
+        String newWord = settings.txtHangman.getText();
+        if(newWord.matches("([a-z]|[A-Z]|\\s)+")){
+           if(!mainApp.mainApp_Controller.hangmanWords.contains(newWord)){
+                mainApp.mainApp_Controller.hangmanWords.add(newWord);
+                modelHangman.addElement(newWord);
+                settings.ltHangman.setModel(modelHangman);
+           }
+        }else
+            JOptionPane.showMessageDialog(settings, "Esta palabra contiene números o caracteres especiales");
     }
-
-    private void clear() {
-        
+    /**************************************************************************\
+     * Clean the lists.
+    \**************************************************************************/
+    private void clear(JList list, DefaultListModel model, ArrayList words) {
+        model.clear();
+        list.setModel(model);
+        words.clear();
     }
 }
